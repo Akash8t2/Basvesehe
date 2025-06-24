b577df658d34dbb55b6677b22f5c845f22a63bea
Akash <158745842+Akash8t2@users.noreply.github.com>
2025-06-24 22:40:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -478,4 +478,4 @@ if __name__ == "__main__":
     for _ in range(THREADS):
         Thread(target=gg_worker, daemon=True).start()
     while True:
-        time.sleep(864000)
+        time.sleep(0.9)
